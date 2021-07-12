cbf91153eb6f57f0b7d92547975c75c7c69f8121
V3D4N7V2 <69084541+V3D4N7V2@users.noreply.github.com>
2021-07-12 14:18:28 +0530

    moved to v2

--- AutoJoinWebex.user.js
+++ AutoJoinWebex.user.js
@@ -3,31 +3,50 @@
 // @namespace V3D4N7V2
 // @match http*://*.webex.com/webappng/sites/*/meeting/info/*
 // @match http*://*.webex.com/webappng/sites/*/meeting/download/*
-// @grant none
+// @grant window.close
 // @version 1.0
 // @author V3D4N7V2
 // @description -
 // @run-at document-start
-// @noframes
 // ==/UserScript==
-if (/https*\:\/\/.*\.webex\.com\/webappng\/sites\/.*\/meeting\/download/.test(window.location.href) && !window.location.search == "?join=true") {
-	window.location.href = window.location.href.replace("download", "info") + "?join=true";
+if (/https*\:\/\/.*\.webex\.com\/webappng\/sites\/.*\/meeting\/download/.test(window.location.href) && !(window.location.search == "?join=true")) {
+	window.location.replace(window.location.href.replace("download", "info"));
 }
-if (/https*\:\/\/.*\.webex\.com\/webappng\/sites\/.*\/meeting\/info/.test(window.location.href) && window.location.search == "?join=true") {
+if (/https*\:\/\/.*\.webex\.com\/webappng\/sites\/.*\/meeting\/info/.test(window.location.href)) {
 	var interval;
 	var url = document.location.origin + "/webappng/api/v1/meetings/" + document.location.pathname.slice(-32);
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var object = JSON.parse(this.responseText);
+			if (object.meetingHost) {
+				clearInterval(interval);
+			}
 			if (object.meetingStarted) {
 				clearInterval(interval);
-				window.location.href = window.location.href.replace("/meeting/info/", "/meeting/download/");
+				joinMeeting();
 			}
 		}
 	};
 	interval = setInterval(function() {
 		xhttp.open("GET", url, true);
 		xhttp.send();
-	}, 5000);
+	}, 1000);
+}
+
+function joinMeeting() {
+	var meetingURL = window.location.href.replace("/meeting/info/", "/meeting/download/");
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var parser = new DOMParser();
+			var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+			var extendedData = htmlDoc.getElementById("extendedData").innerHTML;
+			var object = JSON.parse(extendedData);
+			window.location.href = object2.simpleflowMapping.urlProtocollink;
+		}
+	}
+	xhttp.open("GET", meetingURL, true);
+	xhttp.send();
+	return;
 }
