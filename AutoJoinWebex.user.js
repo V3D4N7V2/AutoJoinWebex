13c5da09b31add50a408a8f779b1d857e12989bb
V3D4N7V2 <69084541+V3D4N7V2@users.noreply.github.com>
2021-07-12 14:58:29 +0530

    minor changes , option to use webapp too

--- AutoJoinWebex.user.js
+++ AutoJoinWebex.user.js
@@ -33,22 +33,26 @@ if (/https*\:\/\/.*\.webex\.com\/webappng\/sites\/.*\/meeting\/info/.test(window
 	interval = setInterval(function() {
 		xhttp.open("GET", url, true);
 		xhttp.send();
-	}, 1000);
+	}, 5000);
 }
 
 function joinMeeting() {
-	var meetingURL = window.location.href.replace("/meeting/info/", "/meeting/download/");
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var parser = new DOMParser();
-			var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
-			var extendedData = htmlDoc.getElementById("extendedData").innerHTML;
-			var object = JSON.parse(extendedData);
-			window.location.href = object2.simpleflowMapping.urlProtocollink;
+	if (document.getElementsByClassName("el-dropdown-menu__item thinclientjoin")[0].getAttribute("aria-checked") == "true") document.getElementById("smartJoinButton-action").click();
+	else {
+		var meetingURL = window.location.href.replace("info", "download");
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var parser = new DOMParser();
+				var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+				var extendedData = htmlDoc.getElementById("extendedData").innerHTML;
+				var object = JSON.parse(extendedData);
+				window.location.href = object2.simpleflowMapping.urlProtocollink;
+			}
 		}
+		xhttp.open("GET", meetingURL, true);
+		xhttp.send();
 	}
-	xhttp.open("GET", meetingURL, true);
-	xhttp.send();
+	
 	return;
 }
